d6739780bd963f7c258e6757af0305b8c27411b0
shais <muhammadshais333@gmail.com>
2023-06-01 09:42:57 -0700

    for loop and while loop completed

---
+++ 5loops.js
@@ -0,0 +1,20 @@
+/* unlike on the basis if-else condtion our statement will be execute similary 
+in for loop we certain condtion which unable the statement to execute multiple time  
+this is something we say difference betweeen loop condition and if-else condtionS 
+*/  
+
+//for loop
+let i=1
+//for(i=1; i<6; i++){
+  //  console.log(i )
+//}
+
+// using for loop print table of 2
+
+//for(i=1; i<11; i++){
+//    console.log('2 x' + i + '='+ i*2)
+//}
+while(i<11){
+    console.log('2 x' + i + '='+ i*2)
+    i++
+}
