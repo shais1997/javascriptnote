bee779d6adab9ef245ddc2b5e563c2f615abe63a
shais <muhammadshais333@gmail.com>
2023-06-02 09:59:27 -0700

    array complete with for loop and functions

---
+++ 8array.js
@@ -0,0 +1,17 @@
+let i;
+let arr=['red', 'green', 'blue']
+for(i=0; i<arr.length; i++){
+    console.log(arr[i])
+}
+// so here we recall the declaration of array and use for loop for array's data
+
+//function that takes an array as input and display all items of this array
+
+function arraT(as){
+    for(i=0; i<as.length; i++){
+        console.log(as[i])
+    }
+}
+
+let colors=['red','greem','boue']
+arraT(colors)
