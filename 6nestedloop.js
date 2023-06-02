f3fabbebf4eb9a7091f1a315c6bc34027edefccd
shais <muhammadshais333@gmail.com>
2023-06-02 09:35:32 -0700

    now first is over and function topic of seconed topic is also cover

--- 6nestedloop.js
+++ 6nestedloop.js
@@ -6,9 +6,27 @@
    { console.log(i + 'x' + j +'='+ i*j)
    }
 }*/
-var cubes = 'ABCDEFG';
+/*var cubes = 'ABCDEFG';
 //styling console output using CSS with a %c format specifier
 for (var i = 0; i < cubes.length; i++) {
     var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
     console.log("%c" + cubes[i], styles)
+}*/
+
+let i;
+for(i=1; i<11; i++){
+    switch(i){
+        case 1:
+        console.log('Gold Medal')
+        break 
+        case 2:
+            console.log('Silver Medal')
+            break
+            case 3:
+                console.log('bronze medal')
+                break
+                default:
+                console.log(i)
+
+    }
 }
---
+++ 7function.js
@@ -0,0 +1,25 @@
+//we say function is a block of code and this code have specific functionality
+//example we hava a function that add two number
+
+function addNum(){
+    let a=4;
+    let b=3;
+    let c=a+b
+    console.log(c)
+}
+addNum()
+
+// so we how function syntax is declare 
+// first we write function than function name in above example function name is addNum
+// inside carly braces is function body
+// next we see how we call the function 
+
+// so inside the braces of function we pass the parameter like this
+
+function addNumb(a,b){
+    let c=a+b
+    console.log(c)
+}
+addNumb(3,6) // and here we pass argument 
+
+// necessary to learn the terminology what is argument parameter invoke of function function body
